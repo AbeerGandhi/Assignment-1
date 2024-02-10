3063edffa891ef027c5668d6d6b6445c7657eb0a
Abeer Gandhi <abeergandhi2@gmail.com>
2024-02-10 14:25:05 +0530

    req-1

---
+++ Assignment 1/req1/test1.js
@@ -0,0 +1,6 @@
+
+import { isLeapYear } from './dateUtil.js';
+
+// Testing one leap year and one non-leap year
+console.log(isLeapYear(2024)); // Leap year
+console.log(isLeapYear(2023)); // Non-leap year
