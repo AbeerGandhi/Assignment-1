66b0ef5cb1f99b2fa1f7d311612c91eb4ec40de3
Abeer Gandhi <abeergandhi2@gmail.com>
2024-02-15 21:15:20 +0530

    req 2

--- reqirement2/readFile.js
+++ reqirement2/readFile.js
@@ -1,10 +1,9 @@
-// Importing the fs module
+
 import { readFile } from 'fs';
 
 // File path
 const filePath = 'poem.txt';
 
-// Reading file contents
 readFile(filePath, 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading file:', err);
